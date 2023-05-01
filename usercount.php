c2c49d47e43aeb3dffafa1bf7f9bb6276b5532ee
nartodono <luthfi.215.ml@gmail.com>
2023-05-01 23:13:11 +0700

    first commit

--- usercount.php
+++ usercount.php
@@ -1,5 +1,5 @@
 <?php
-
+// Koneksi ke database
 $host = 'db';
 
 $user = 'user';
@@ -10,30 +10,24 @@ $mydatabase = 'MYSQL_DATABASE';
 
 $conn = new mysqli($host, $user, $pass, $mydatabase);
 
-$sql = 'SELECT * FROM users';
-
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
-    }
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
 
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->id . " " . $user->nama . " " . $user->alamat . " " . $user->jabatan;
-    echo "<br>";
-}
-
+// Query untuk menghitung jumlah user
 $query = "SELECT COUNT(*) as total_users FROM users";
 $result = $conn->query($query);
 
 // Ambil hasil query
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<br>";
         echo "Jumlah user: " . $row["total_users"];
     }
 } else {
     echo "Tidak ada data user.";
 }
+
+// Tutup koneksi database
+$conn->close();
 ?>
